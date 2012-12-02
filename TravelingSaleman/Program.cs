161cbdba9dac4e84ce99bc3a259795c19c180ea6
Binh Tran <gspeedman@gmail.com>
2012-12-01 18:26:56 -0800

    Working with small and medium

--- TravelingSaleman/Program.cs
+++ TravelingSaleman/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TravelingSaleman
 {
@@ -43,12 +45,13 @@ namespace TravelingSaleman
             {"Pendleton", 20}, {"Portland", 21}, {"Redmond", 22}, {"Roseburg", 23}, {"Salem", 24}, 
             {"Springfield", 25}, {"The.Dalles", 26}, {"Tillamook", 27}, {"Woodburn", 28},
         };
-        static int total = 29;
+        public const int total = 29;
         static int[,] mileage = new int[total, total];
-        
+        public const int RUNTIME_MAX = 60;
+
         static int ReadFileFillMileage()
         {
-            System.IO.StreamReader file = new System.IO.StreamReader("..\\..\\oregon-mileage.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader(@"..\..\oregon-mileage.txt");
             string line;
             int cnt = 0;
 
@@ -66,7 +69,7 @@ namespace TravelingSaleman
             {
                 for (int j = 0; j < total; ++j)
                 {
-                    Debug.Assert( mileage[i,j] != 0 || mileage[j,i] != 0 || i == j, i.ToString() + " " + j.ToString());
+                    Debug.Assert(mileage[i, j] != 0 || mileage[j, i] != 0 || i == j, i.ToString() + " " + j.ToString());
                     if (mileage[i, j] == 0)
                     {
                         mileage[i, j] = mileage[j, i];
@@ -80,7 +83,7 @@ namespace TravelingSaleman
                 }
             }
 
-            System.IO.StreamWriter outfile = new System.IO.StreamWriter("..\\..\\milage-matrix.txt");
+            System.IO.StreamWriter outfile = new System.IO.StreamWriter(@"..\..\milage-matrix.txt");
             for (int i = 0; i < total; ++i)
             {
                 for (int j = 0; j < total; ++j)
@@ -95,15 +98,151 @@ namespace TravelingSaleman
 
         static void FillMileage()
         {
+            using(System.IO.StreamReader mileageFile = new System.IO.StreamReader(@"..\..\milage-matrix.txt"))
+            {
+                for (int i = 0; i < total; ++i)
+                {
+                    string[] fields = mileageFile.ReadLine().Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+                    Debug.Assert(fields.Length == total, "Field length != total");
+                    for (int j = 0; j < total; ++j)
+                    {
+                        mileage[i, j] = Convert.ToInt32(fields[j]);
+                    }
+                }
+            }
 
+            //PrintArr(mileage);
+        }
+        
+        [Conditional("DEBUG")]
+        private static void PrintArr(int[,] mileage)
+        {
+            for (int i = 0; i < total; ++i)
+            {
+                for (int j = 0; j < total; ++j)
+                {
+                    Console.Write(mileage[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
 
+        static void Shuffle(int[] arr)
+        {
+            Random prng = new Random();
+
+            for (int i = arr.Length - 2; i > 0; --i)       // start from the second-to-last, end at the second city
+            {
+                int j = prng.Next(1, i + 1);
+                swap(arr, i, j);
+            }
+            Debug.Assert(arr[0] == arr[arr.Length - 1]);
+        }
+
+        static public void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static int Score(int[] route)
+        {
+            int score = 0;
+            for (int i = 1; i < route.Length; ++i)
+            {
+                score += mileage[route[i - 1], route[i]];
+            }
+            return score;
+        }
+
+        static TourResult Search(object Data)
+        {
+            Debug.Assert(Data is TourData);
+            TourData data = (TourData)Data;
+            DateTime deadline = DateTime.Now.AddSeconds(RUNTIME_MAX);
+            TourResult result = new TourResult(new int[data.CitiesInTour.Length], Int32.MaxValue);
+            do
+            {
+                Shuffle(data.CitiesInTour);
+                while (true)
+                {
+                    int swapScore = Score(data.CitiesInTour);
+                    int swapI = -1;
+                    for (int i = 2; i < data.CitiesInTour.Length - 1; ++i)
+                    {
+                        swap(data.CitiesInTour, i - 1, i);
+                        if (Score(data.CitiesInTour) < swapScore)
+                        {
+                            swapScore = Score(data.CitiesInTour);
+                            swapI = i;
+                        }
+                        swap(data.CitiesInTour, i - 1, i);
+                    }
+
+                    Debug.Assert(swapI > 1 && swapI < data.CitiesInTour.Length - 1 || swapI == -1, swapI.ToString());
+                    if (swapI != -1)
+                    {
+                        swap(data.CitiesInTour, swapI, swapI - 1);
+                    }
+                    else        // local maximum reached
+                    {
+                        int n = Score(data.CitiesInTour);
+                        if (n < result.Score)
+                        {
+                            Array.Copy(data.CitiesInTour, result.TourRoute, data.CitiesInTour.Length);
+                            result.Score = n;
+                        }
+                        break;
+                    }
+                }
+            } while (DateTime.Now <= deadline);
+            return result;
+        }
 
         static void Main(string[] args)
         {
             //GetNames();
-            Console.WriteLine(ReadFileFillMileage());
+            //Console.WriteLine(ReadFileFillMileage());
+            FillMileage();
+            TourData tourInfo = new TourData(ReadTour(args[0]).ToArray(), mileage);
+            Task<TourResult>[] searchers = new Task<TourResult>[Environment.ProcessorCount / 2];
+            TaskFactory tf = new TaskFactory();
+            for (int i = 0; i < searchers.Length; ++i)
+            {
+                searchers[i] = Task.Factory.StartNew<TourResult>(Search, ((ICloneable)tourInfo).Clone());
+            }
+            Task.WaitAll(searchers);
+            TourResult[] results = new TourResult[searchers.Length];
+            for (int i = 0; i < searchers.Length; ++i)
+            {
+                results[i] = searchers[i].Result;
+            }
+            Array.Sort(results);
+            Console.WriteLine(results[0].toString(cities));
             Console.ReadLine();
+        }
+
+        private static List<int> ReadTour(string p)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(p);
+            string line;
+            List<int> citiesInTour = new List<int>();
+            while ((line = file.ReadLine()) != null)
+            {
+                try
+                {
+                    citiesInTour.Add(cities[line]);
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine(line + " city not founded");
+                    return null;
+                }
+            }
+            citiesInTour.Sort();
+            citiesInTour.Add(citiesInTour[0]);
+            return citiesInTour;
         }
     }
 }
